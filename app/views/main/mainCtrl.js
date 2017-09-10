d75da2d69b62e80a2d9bbb733066cd69723798c2
Lloyd Grubham <grubham@gmail.com>
2017-09-10 01:14:43 -0600

    Added pictures.

--- app/views/main/mainCtrl.js
+++ app/views/main/mainCtrl.js
@@ -1,3 +1,12 @@
-angular.module('app').controller('mainCtrl', function($scope ){
-    $scope.test = 'ciao'
+angular.module('app').controller('mainCtrl', function($scope, $location, $anchorScroll){
+    $scope.showNav = false;
+    $scope.toggleNav = function() {
+        $scope.showNav = !$scope.showNav;
+    }
+    $scope.scrollTo = function(elem) {
+        $location.hash(elem);
+        $anchorScroll.yOffset = 80;
+        $anchorScroll();
+        $scope.showNav = false;
+    }
 });
